921c16a0427c6406ed732bbf4e0bfa082f0fd3d8
Thirupathi <115494239+Thirupathi-702@users.noreply.github.com>
2024-02-11 23:47:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const BrandName = require('./model');
+
+const app = express();
+
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://gudithirupathi02:thiru@database01.qe0b4xx.mongodb.net/?retryWrites=true&w=majority').then(
+    ()=>console.log("DBconnectd")
+    ).catch(err=>console.log(err))
+
+
+app.post('/addbrands',async (req,res) => {
+    const {brandname} = req.body ;
+    try{
+        const newData = new BrandName({brandname});
+        await newData.save();
+        return res.json(await BrandName.find())
+    }
+    catch(err){
+        console.log(err.message);
+    }
+})
+
+app.get('/getallbrands',async (req,res) => {
+    try {
+        const allData = await BrandName.find();
+        return res.json(allData);
+    }
+    catch(err){
+        console.log(err.message);
+    }
+})
+
+app.get('/getallbrands/:id', async (req,res) => {
+    try{
+        const Data = await BrandName.findById(req.params.id)
+        return res.json(Data);
+    }
+    catch(err){
+        console.log(err.message);
+    }
+})
+
+app.delete('/deletebrand/:id',async (req,res)=>{
+    try{
+        await BrandName.findByIdAndDelete(req.params.id);
+        return res.json(await BrandName.find())
+    }
+    catch(err){
+        console.log(err.message);
+    }
+})
+
+app.listen(5000,()=>console.log('Server running...'))
